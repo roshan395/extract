4d2d8bc4f1d76ed5c85de7e43eece6a3bbe024c2
roshan395 <roushanraj395@gmail.com>
2024-12-07 23:56:44 +0530

    Fix port binding for Render

--- keyapi.py
+++ keyapi.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -46,4 +47,6 @@ def get_instrument():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to 0.0.0.0 and use the port provided by Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
